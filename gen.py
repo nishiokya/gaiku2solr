7a97d99a756621b2a426b44b489011e4713f1228
nishiokya <nishiokya@gmail.com>
2020-08-06 00:35:53 +0900

    modify

--- gen.py
+++ gen.py
@@ -10,7 +10,7 @@ import io
 
 
 def getAddressMaster():
-    global ooazadict 
+    global ooazadict ,citydict
     url = "https://raw.githubusercontent.com/geolonia/japanese-addresses/master/data/latest.csv"
     r = requests.get( url)
     print("get address master {}".format(r.status_code))
@@ -19,10 +19,13 @@ def getAddressMaster():
     for line in reader:
         #print(line[9])
         address = line[1]+line[5]+line[9]
+        city = line[1]+line[5]
         #print(address)
         ooazadict[address] = line
+        citydict[city] = line
         
 ooazadict = {}
+citydict = {}
 getAddressMaster()
 
 url = "http://localhost:8983/solr/address/update?commit=true"
@@ -40,6 +43,7 @@ with open("/Users/nishiokatakaaki/work/solr-8.2.0/23000-17.0a/23_2018.csv", enco
     for line in reader:
         
         address = line[0]+line[1]+line[2]
+        city = line[0]+line[1]
         #print(address)
         if address in ooazadict.keys():
             acode = ooazadict[address]
@@ -68,11 +72,58 @@ with open("/Users/nishiokatakaaki/work/solr-8.2.0/23000-17.0a/23_2018.csv", enco
                 "address":address
 
             }
-        else:
-            print(address)
-        i+=1
-        additems.append((item))
+            i+=1
+            
+        elif city in citydict.keys():
+            acode = citydict[city]
+            item = {
+                "id" : i,
+                "prefname":line[0],
+                "cityname":line[1],
+                "ooazaname":line[2],
+                "azaname":line[3],
+                "gaikuname":line[4],
+                "zahyou":line[5],
+                "x":line[6],
+                "y":line[7],
+                "lat":line[8],
+                "lon":line[9],
+                "jyuukyo":line[10],
+                "daihyou":line[11],
+                "history1":line[12],
+                "history2":line[13],
+                "lonlat":str(line[9])+","+str(line[8]),
+                "prefcode":city[0],
+                "citycode":city[4],
+                #"ooazacode":acode[8],
+                "address":address
 
+            }
+        else:
+            item = {
+                "id" : i,
+                "prefname":line[0],
+                "cityname":line[1],
+                "ooazaname":line[2],
+                "azaname":line[3],
+                "gaikuname":line[4],
+                "zahyou":line[5],
+                "x":line[6],
+                "y":line[7],
+                "lat":line[8],
+                "lon":line[9],
+                "jyuukyo":line[10],
+                "daihyou":line[11],
+                "history1":line[12],
+                "history2":line[13],
+                "lonlat":str(line[9])+","+str(line[8]),
+                
+                "address":address
+
+            }
+        additems.append((item))
+            #print(address)
+        
 data = json.dumps(additems).encode("utf-8")
 #print(data)
 headers = {"Content-Type" : "application/json; charset=utf-8"}
